847599c74fa84ebd5dcbd1c672a5a01b13c02401
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-10-07 09:55:46 -0300

    feat (auth components): create the auth components

---
+++ src/ui/components/deleteUser/deleteUserFunction.js
@@ -0,0 +1,24 @@
+require("dotenv").config({ quiet: true });
+
+async function deleteUser(id, token) {
+  try {
+    const response = await fetch(`${process.env.API_URL}/users/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ null: null }),
+    });
+
+    const result = await response.json();
+    return {
+      response,
+      result,
+    };
+  } catch (err) {
+    return err;
+  }
+}
+
+exports.deleteUser = deleteUser;
