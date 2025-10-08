eed055f4012de1b45f89c1a5443b7068500bfa54
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-10-08 18:07:27 -0300

    initiate the signup component create

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
+import { SignInForm } from "@/ui/components/auth/signIn/signInForm";
+
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center h-lvh">
-      <h1 className="font-bold text-2xl">Seja bem vindo ao GHS Sistema</h1>
-      <p>Em construção, estará pronto em breve</p>
-      <p>Teste</p>
+    <div className="flex flex-col p-8 items-center justify-center min-h-lvh h-full">
+      <SignInForm />
     </div>
   );
 }
