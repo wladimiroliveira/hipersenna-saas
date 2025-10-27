bc338e97a6006a3c82ce1968a3518b166a155b3b
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-10-27 10:53:15 -0300

    feat (create users): add the create users page

--- src/app/home/(modules)/admin/users/create/page.js
+++ src/app/home/(modules)/admin/users/create/page.js
@@ -3,7 +3,10 @@ import { SignUpForm } from "@/app/ui/models/auth/signUp/signUpForm";
 export default function Page() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center w-full h-lvh p-8">
+      <div className="p-12">
+        <h1 className="text-4xl text-primaria font-bold">Criar usuário</h1>
+      </div>
+      <div className="flex flex-row w-full h-[70vh] pt-0 p-8">
         <SignUpForm />
       </div>
     </div>
