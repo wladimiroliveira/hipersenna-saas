282433a5970e415b5d1e5f4a2491abddfad47fd1
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-10-27 10:49:55 -0300

    feat (home damin): create the home admin

--- src/app/home/(modules)/admin/page.js
+++ src/app/home/(modules)/admin/page.js
@@ -1,7 +1,15 @@
+import { ModulesContainer } from "@/app/ui/components/modulesContainer";
+import modules from "@/lib/files/modules.json";
+
 export default function Page() {
   return (
     <div>
-      <h1>Admin Home Page</h1>
+      <div className="p-12">
+        <h1 className="text-4xl text-primaria font-bold">Admin</h1>
+      </div>
+      <div className="pl-12 pr-12">
+        <ModulesContainer title="Módulos Admin" modules={modules[0].submodules} />
+      </div>
     </div>
   );
 }
