e34bf0ad59f5db9d296e367765f0447ea48b6236
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-10-27 17:44:32 -0300

    feat (database): add dev database headler

---
+++ src/infra/database.js
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+export default async function query(queryObjects) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  try {
+    await client.connect();
+    const result = await client.query(queryObjects);
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
