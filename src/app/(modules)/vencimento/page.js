3e7a650c8df0c95db0b1d139df785ee40c7c5837
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-10-31 16:59:49 -0300

    feat (vencimento): create the validity modules container

--- src/app/(modules)/vencimento/page.js
+++ src/app/(modules)/vencimento/page.js
@@ -1,7 +1,15 @@
+import { ModulesContainer } from "@/app/ui/components/modulesContainer";
+import modules from "@/lib/files/modules.json";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Vencimento</h1>
+    <div className="pl-4 pr-4">
+      <div className="pt-12 pb-12">
+        <h1 className="text-4xl text-primaria font-bold">Vencimento</h1>
+      </div>
+      <div>
+        <ModulesContainer title="Módulos" modules={modules[1].submodules} />
+      </div>
     </div>
   );
 }
