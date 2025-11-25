3db435825250495d05ab7de9f5298889aeb745f7
Wladimir Oliveira <wladimir4019@gmail.com>
2025-11-24 22:13:00 -0300

    init creation of `role` endpoint

---
+++ src/app/api/v1/roles/route.js
@@ -0,0 +1,28 @@
+import { cookies } from "next/headers";
+
+export async function GET(request) {
+  const authorization = request.headers.get("authorization");
+  let token = authorization?.replace("Bearer ", "");
+  if (!token) {
+    const cookieStore = await cookies();
+    token = cookieStore.get("token")?.value;
+  }
+  try {
+    const responseResult = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/roles`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const responseValue = await responseResult.json();
+    return Response.json([
+      {
+        status: responseResult.status,
+        ...responseValue,
+      },
+    ]);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
---
+++ src/tests/integration/roles/get.test.js
@@ -0,0 +1,24 @@
+test("GET to /api/v1/roles should return 200", async () => {
+  const signIn = await fetch("http://localhost:3000/api/v1/signin", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify([
+      {
+        username: process.env.BOOTSTRAP_ADMIN_USER,
+        password: process.env.BOOTSTRAP_ADMIN_PASSWORD,
+      },
+    ]),
+  }).then((r) => r.json());
+  let token = signIn[0].token;
+
+  const responseResult = await fetch("http://localhost:3000/api/v1/roles", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const responseValue = await responseResult.json();
+  console.log(responseValue);
+});
