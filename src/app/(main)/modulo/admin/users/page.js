7df7c71c479502023ec461725cb1253154f76e95
Wladimir Oliveira <wladimir4019.com>
2025-12-02 11:43:03 -0300

    fix `module` render

--- src/app/(main)/modulo/admin/users/page.js
+++ src/app/(main)/modulo/admin/users/page.js
@@ -2,22 +2,15 @@
 
 import { ModulesContainer } from "@/components/views/modulesContainer.view";
 import modules from "@/files/modules.json";
-import { useUserInfo } from "@/lib/auth-client";
 
 export default function Page() {
-  const userInfo = useUserInfo();
-
-  if (!userInfo) {
-    return <div>Carregando...</div>;
-  }
-
   return (
     <div className="pl-4 pr-4">
       <div className="pt-12 pb-12">
         <h1 className="text-4xl text-primaria font-bold">Usuários</h1>
       </div>
       <div>
-        <ModulesContainer title="Módulos Admin" modules={modules[0].submodules[0].submodules} access={userInfo} />
+        <ModulesContainer title="Módulos Admin" modules={modules[0].submodules[0].submodules} />
       </div>
     </div>
   );
