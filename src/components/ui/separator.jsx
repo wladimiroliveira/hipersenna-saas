57a4ac08514a59bb9b1251485d62c595790f6b9a
Wladimir Oliveira <wladimir4019.com>
2026-01-05 17:50:29 -0300

    add `style lint` action

--- src/components/ui/separator.jsx
+++ src/components/ui/separator.jsx
@@ -1,16 +1,11 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import * as React from "react";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Separator({
-  className,
-  orientation = "horizontal",
-  decorative = true,
-  ...props
-}) {
+function Separator({ className, orientation = "horizontal", decorative = true, ...props }) {
   return (
     <SeparatorPrimitive.Root
       data-slot="separator"
@@ -18,10 +13,11 @@ function Separator({
       orientation={orientation}
       className={cn(
         "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className
+        className,
       )}
-      {...props} />
+      {...props}
+    />
   );
 }
 
-export { Separator }
+export { Separator };
